5db4e8ce9c087186215b47ab9d70d51fe2b12aa9
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2022-03-07 15:21:46 +0200

    render list

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
+import StatisticsItem from './StatisticsItem';
 
 function Statistics({ title, stats }) {
   return (
@@ -7,25 +8,9 @@ function Statistics({ title, stats }) {
       {title && <h2 className={styles.title}>{title}</h2>}
 
       <ul className="stat-list">
-        <li className="item">
-          <span className="label">.docx</span>
-          <span className="percentage">4%</span>
-        </li>
-
-        <li className="item">
-          <span className="label">.mp3</span>
-          <span className="percentage">14%</span>
-        </li>
-
-        <li className="item">
-          <span className="label">.pdf</span>
-          <span className="percentage">41%</span>
-        </li>
-
-        <li className="item">
-          <span className="label">.mp4</span>
-          <span className="percentage">12%</span>
-        </li>
+        {stats.map(item => {
+          return <StatisticsItem data={item} key={item.id} />;
+        })}
       </ul>
     </section>
   );
