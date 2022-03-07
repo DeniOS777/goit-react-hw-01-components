02fb82afdd72f0f8c7101fe154830fbb1476494c
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2022-03-07 20:13:36 +0200

    upd style

--- src/components/Statistics/StatisticsItem.jsx
+++ src/components/Statistics/StatisticsItem.jsx
@@ -2,8 +2,15 @@ import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
 function StatisticsItem({ data }) {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
   return (
-    <li className={styles.item}>
+    <li
+      className={styles.item}
+      style={{ backgroundColor: getRandomHexColor() }}
+    >
       <span className={styles.label}>{data.label}</span>
       <span className={styles.percentage}>{data.percentage}%</span>
     </li>
