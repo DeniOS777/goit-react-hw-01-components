fffbc6b144a440b5c48bcd6ac7f473e16867e0d0
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2022-03-12 21:50:47 +0200

    upd styles friends

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -4,7 +4,7 @@ import styles from './FriendList.module.css';
 function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={styles.item}>
-      <span className={styles.status}></span>
+      <span className={styles[isOnline]}></span>
       <img
         className={styles.avatar}
         src={avatar}
